771a351e1039c851e983bea54d7097a0d64eec8f
Benjamin Chen <ben@nat-oitwireless-inside-vapornet100-a-2654.princeton.edu>
2014-04-29 17:09:26 -0400

    prettified

--- Hashtagger/urls.py
+++ Hashtagger/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hr/', include("hrapp.urls")),
+    url(r'^$', include("hrapp.urls")),
 )
