122619183b3966432759ad1cea76b9a95740643d
Benjamin Chen <ben@nat-oitwireless-inside-vapornet100-a-2654.princeton.edu>
2014-04-29 15:39:43 -0400

    changed settings

--- Hashtagger/settings.py
+++ Hashtagger/settings.py
@@ -8,9 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
